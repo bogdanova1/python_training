7ce10c2a436019dd1ec80b0b6e8cdc31c9b04b39
Natalia Bogdanova <Natalia.Bogdanova@firstlinesoftware.com>
2020-01-23 16:57:31 +0300

    Добавлено редактирование группы

--- fixture/group.py
+++ fixture/group.py
@@ -35,6 +35,20 @@ class GroupHelper:
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
 
+    def edit_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_name("edit").click()
+        # fill first fild of group form
+        text = wd.find_element_by_name("group_name").text
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").send_keys(text+"_2")
+        # submit edition
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+
     def open_groups_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
