283b23fc7d18ad3b109b9f4b4ebc90a803863069
Natalia Bogdanova <Natalia.Bogdanova@firstlinesoftware.com>
2020-01-27 20:10:46 +0300

    Повышена интеллектуальность логина и логаута

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,14 +3,11 @@ from model.group import Group
 
 def test_modify_group_name(app):
     app.group.modify_first_group(Group(name="NewGroupName"))
-    app.session.logout()
 
 
 def test_modify_group_header(app):
     app.group.modify_first_group(Group(header="NewGroupHeader"))
-    app.session.logout()
 
 
 def test_modify_group_footer(app):
     app.group.modify_first_group(Group(footer="testGroupFooter"))
-    app.session.logout()
